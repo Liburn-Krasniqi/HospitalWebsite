acda56953f0cc96163c00727ba52e95824f2e827
Liburn Krasniqi <krasniqiliburn@gmail.com>
2024-10-26 21:52:43 +0200

    refactorGet

--- Server/methods/get-request.js
+++ Server/methods/get-request.js
@@ -1,37 +1,39 @@
 import {pool} from '../database.js';
 
 export async function getReq(req,res){
-    let baseUrl = req.url.substring(0, req.url.lastIndexOf("/"))
-    let id = Number(req.url.split("/")[3]);
-    // const regexV4 = new RegExp(
-    //      /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
-    // );
+    //let baseUrl = req.url.substring(0, req.url.lastIndexOf("/"))
+    let id = req.url.split("/")[2];
+    let query;
+    
     try{ 
-        if(req.url === "/api/persons"){
-            const [result] = await pool.query("SELECT * FROM person;");
-            res.statusCode = 200;
+        switch(req.url){
+            case "/persons":
+                if(typeof id ==='number'){
+                    query =[`SELECT * FROM person WHERE UserId = ?;`, [id]];
+                }else{
+                    query = [`SELECT * FROM person;`,[null]];//very ugly solution to not deconstruct the string into chars :(
+                }
+            break;
+            case "/patients":
+                query = [`SELECT pe.UserId, pe.FirstName, pe.Birthday, pe.Sex, pe.Phone, pe.Email, pe.City, pe.City, pe.Street, pe.AddressNr, pe.Roli
+                         FROM patient p ,person pe 
+                         where p.UserId = pe.UserId;`,[null]];
+            break;
+            default:
+                res.writeHead(404, {"Content-Type" : "application/json"});
+                res.end(JSON.stringify({title: "Not Found", message: "Something wrong with your request sowyy!"}));
+            }
+        const [result] = await pool.query(...query);
+        res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
         res.write(JSON.stringify(result));
-        res.end();
-    }else if(baseUrl === "/api/persons" && (typeof id =='number')){
-        const [[result]] = await pool.query(`
-            SELECT *
-            FROM person
-            WHERE UserId = ?;`,[id]);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-        res.write(JSON.stringify(result));
-        res.end();
-    }else{
-        res.writeHead(404, {"Content-Type" : "application/json"});
-        res.end(JSON.stringify({title: "Not Found", message: "Something wrong with your request sowyy!"}));
-    }
+        res.end();    
     }catch(err){
         console.log("QIKY OSHT ERRORI:" + err);
         res.writeHead(404, {"Content-Type" : "application/json"});
-            res.end(JSON.stringify({
-                title: "Not Found",
-                message: "Something wrong with your request."
-                }));
+        res.end(JSON.stringify({
+            title: "Not Found",
+            message: "Something wrong with your request."
+        }));
     }
 };
